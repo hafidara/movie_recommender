17785eeef0a027e299ce5a6933ed6e87b8e36cb0
Hafida Rahmane <59095444+hafidara@users.noreply.github.com>
2024-06-24 13:23:01 +0100

    Updated

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -3,28 +3,20 @@ from .models import Movie
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# HINT: Create a view to provide movie recommendations list for the HTML template
-
 def movie_recommendation_view(request):
     if request.method == "GET":
         context = generate_movies_context()
         return render(request, 'movierecommender/movie_list.html', context)
 def generate_movies_context():
     context = {}
-    # Show only movies in recommendation list
-    # Sorted by vote_average in desc
-    # Get recommended movie counts
     recommended_count = Movie.objects.filter(
         recommended=True
     ).count()
-    # If there are no recommended movies
     if recommended_count == 0:
-        # Just return the top voted and unwatched movies as popular ones
         movies = Movie.objects.filter(
             watched=False
         ).order_by('-vote_count')[:30]
     else:
-        # Get the top voted, unwatched, and recommended movies
         movies = Movie.objects.filter(
             watched=False
         ).filter(
